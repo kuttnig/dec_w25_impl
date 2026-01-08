225779bee10b42026d9f200ad5bb771120d72492
Kevin Ernst Kuttnig <94113871+kuttnig@users.noreply.github.com>
2026-01-08 18:47:13 +0100

    add product list component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 export default function App() {
+  return <ProductList />;
+}
+
+function ProductList() {
   const productListQuery = useQuery({
     queryKey: ['productListQuery'],
     queryFn: async () => {
@@ -10,15 +14,35 @@ export default function App() {
     },
   });
 
-  let queryMsg = '';
   if (productListQuery.isPending) {
-    queryMsg = 'fetching data';
-  } else if (productListQuery.isError) {
-    queryMsg = 'failed to fetch data';
-  } else {
-    queryMsg = JSON.stringify(productListQuery.data);
+    return <p>fetching product list</p>;
+  } if (productListQuery.isError) {
+    return <p>failed to fetch product list</p>;
   }
 
-  // todo: render product list
-  return <p>{queryMsg}</p>;
+  const { products } = productListQuery.data;
+  const productProps = Object.keys(products[0]);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {productProps.map((prop) => <th key={prop}>{prop}</th>)}
+          <th>offers</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((prod) => (
+          <tr key={prod.prodId}>
+            {
+              productProps.map((prop) => <td key={`${prod.prodId}-${prop}`}>{prod[prop]}</td>)
+            }
+            <td>
+              <button type="button">view</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
