7b3a2d15df35640933f034d2f59b6775a407b5d1
Kevin Ernst Kuttnig <94113871+kuttnig@users.noreply.github.com>
2026-01-08 17:21:23 +0100

    add list products endpoint

--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -1,26 +1,41 @@
 import express from 'express';
+import Ajv from 'ajv';
+
 import Product from '../db/model/Product.js';
 
+import listReqSchema from '../schemas/products/list_products_req.schema.json' with { type: 'json' };
+import listResSchema from '../schemas/products/list_products_res.schema.json' with { type: 'json' };
+
 const router = express.Router();
+const ajv = new Ajv();
 
-router.post('/List', async (req, res) => {
-  try {
-    // todo: run schema check on req body
+const validateListReq = ajv.compile(listReqSchema);
+const validateListRes = ajv.compile(listResSchema);
 
-    // todo: run graphQL query
-
-    // todo: run schema check on res body
-
-    // todo: rm placeholder
-    const productsFound = await Product.find()
-      .populate('categories')
-      .populate('offers')
-      .exec();
-
-    res.json(productsFound);
-  } catch (e) {
-    res.status(500).json({ msg: 'oopsie whoopsie' });
+router.post('/List', (req, res, next) => {
+  if (!validateListReq(req.body)) {
+    res.status(400).json({ msg: 'req schema mismatch' });
+    return;
   }
+  next();
+}, async (req, res) => {
+  let productList = await Product.aggregate([
+    {
+      $project: {
+        _id: 0,
+        prodId: { $toString: '$_id' },
+        name: 1,
+        description: 1,
+      }
+    }
+  ]);
+  productList = {products: productList}
+
+  if (!validateListRes(productList)) {
+    res.status(500).json({ msg: 'res schema mismatch' });
+    return;
+  }
+  res.json(productList);
 });
 
 export default router;
