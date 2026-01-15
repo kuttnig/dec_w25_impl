ddfcc531f58f75dad72f4db1ed251b72ab8cc64a
Kevin Ernst Kuttnig <94113871+kuttnig@users.noreply.github.com>
2026-01-15 16:03:09 +0100

    add status limit route

--- backend/src/routes/limit.js
+++ backend/src/routes/limit.js
@@ -7,12 +7,16 @@ import User from '../db/model/User.js';
 
 import placeReqSchema from '../schemas/limits/place_limit_req.schema.json' with { type: 'json' };
 import placeResSchema from '../schemas/limits/place_limit_res_ack.schema.json' with { type: 'json' };
+import statusReqSchema from '../schemas/limits/get_limit_status_req.schema.json' with { type: 'json' };
+import statusResSchema from '../schemas/limits/get_limit_status_res.schema.json' with { type: 'json' };
 
 const router = express.Router();
 const ajv = new Ajv();
 
 const validatePlaceReq = ajv.compile(placeReqSchema);
 const validatePlaceRes = ajv.compile(placeResSchema);
+const validateStatusReq = ajv.compile(statusReqSchema);
+const validateStatusRes = ajv.compile(statusResSchema);
 
 router.post('/Place', (req, res, next) => {
   if (!validatePlaceReq(req.body)) {
@@ -44,6 +48,27 @@ router.post('/Place', (req, res, next) => {
   }
 
   res.json(limitAck);
+});
+
+router.post('/Status', (req, res, next) => {
+  if (!validateStatusReq(req.body)) {
+    res.status(400).json({ msg: 'req schema mismatch' });
+    return;
+  }
+  next();
+}, async (req, res) => {
+  const {limId} = req.body;
+
+  const limit = await Limit.findById(new mongoose.Types.ObjectId(limId));
+
+  const statusRes = {limId, status: limit.status}
+  
+  if (!validateStatusRes(statusRes)) {
+    res.status(500).json({ msg: 'res schema mismatch' });
+    return;
+  }
+
+  res.json(statusRes);
 });
 
 export default router;
